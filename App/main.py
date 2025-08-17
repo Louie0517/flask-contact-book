53d7699815b697a6d48223ba9ab0961b2a7f6ac1
Brent Casas <brentcasas0517@gmail.com>
2025-08-17 15:13:28 +0800

    Organizing folders

--- App/main.py
+++ App/main.py
@@ -680,5 +680,5 @@ def time_logs_page():
     
 if __name__ == "__main__":
     database_init()
-    app.run(debug=True)
-    #app.run(host="localhost", port=5000)
+    # app.run(debug=True)
+    app.run(host="localhost", port=5000)
--- App/qr.py
+++ App/qr.py
@@ -1,7 +1,7 @@
 import qrcode
 
-employee_id = "70755421"
-base_url = "https://hoped-text-milk-bloomberg.trycloudflare.com"  
+employee_id = "88139791"
+base_url = "https://latin-shuttle-lopez-gabriel.trycloudflare.com" 
 full_url = f"{base_url}/scan?id={employee_id}"
 
 qr = qrcode.make(full_url)
